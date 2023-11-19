4e4f5e953b392ec1919ae35d776e0d1c110429e8
robotanh <robotanh15@gmail.com>
2023-11-19 20:26:07 +0700

    FINISH

--- LAB4_exe/Core/Src/fsm.c
+++ LAB4_exe/Core/Src/fsm.c
@@ -14,6 +14,7 @@ void command_parser_fsm (){
 		if(buffer[curr_index_buffer]=='!') {
 			curr_index_buffer++;
 			index_command = 0;
+			command_data[0] = '\0';
 			command_flag = 0;
 			command_state = Body;
 		}
@@ -56,10 +57,10 @@ void uart_communiation_fsm () {
 			//HAL_ADC_PollForConversion(&hadc1, 20);
 			ADC_value = HAL_ADC_GetValue (&hadc1);
 			sprintf( strr , "\r\n!ADC=%lu#\r\n", ADC_value );
-			HAL_UART_Transmit (&huart2 , (uint8_t *) strr, strlen( strr), HAL_MAX_DELAY);
+			HAL_UART_Transmit (&huart2 , (uint8_t *) strr, strlen( strr), 20);
 			command_flag = 0;
 			uart_state = Wait;
-			set_Timer1(1000);
+			set_Timer1(300);
 		}
 		break;
 	case Wait:
@@ -67,11 +68,12 @@ void uart_communiation_fsm () {
 			uart_state = Init;
 			command_flag = 0;
 		}
-		else if (timer1_flag){
-			sprintf ( strr , "\r\n!ADC=%lu#\r\n", ADC_value );
-			HAL_UART_Transmit (&huart2 , (uint8_t *) strr, strlen( strr), HAL_MAX_DELAY);
-			set_Timer1(1000);
+		else if (timer1_flag == 1){
+			//sprintf ( strr , "\r\n!ADC=%lu#\r\n", ADC_value );
+			HAL_UART_Transmit (&huart2 , (uint8_t *) strr, strlen( strr), 20);
+			set_Timer1(300);
 		}
 		break;
 	}
+
 }
