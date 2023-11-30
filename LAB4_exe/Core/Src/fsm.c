9d3e64a2556c175dcb3ff63023895b91d8b0bf60
robotanh <robotanh15@gmail.com>
2023-11-30 09:55:33 +0700

    done

--- LAB4_exe/Core/Src/fsm.c
+++ LAB4_exe/Core/Src/fsm.c
@@ -7,6 +7,15 @@ enum Command_state {
 		Body
 };
 static enum Command_state command_state = Begin;
+char strr[30];
+uint32_t ADC_value;
+enum Uart_state {
+		Init,
+		Wait,
+		Nothing
+};
+static enum Uart_state uart_state = Nothing;
+int flag =0;
 uint8_t index_command = 0;
 void command_parser_fsm (){
 	switch (command_state){
@@ -17,23 +26,43 @@ void command_parser_fsm (){
 			command_data[0] = '\0';
 			command_flag = 0;
 			command_state = Body;
+			if(curr_index_buffer==30) curr_index_buffer=0;
 		}
 		break;
 	case Body:
-		if(buffer[curr_index_buffer]=='!') break;
+		if(buffer[curr_index_buffer]=='!'){
+			//command_flag=0;
+			//uart_state = Init;
+			curr_index_buffer++;
+			index_command = 0;
+			command_data[0] = '\0';
+			command_flag = 0;
+			if(curr_index_buffer==30) curr_index_buffer=0;
+			break;
+		}
 		if(buffer[curr_index_buffer]=='#') {
 
 			command_data[index_command] = '\0';
 			if(strcmp(command_data, "RST") == 0) {
 				command_flag = 1;
+				uart_state = Init;
+				flag =1;
 			}
 			else if(strcmp(command_data,"OK")  == 0) {
-				command_flag = 1;
+				command_flag = 0;
+				flag=0;
+				uart_state = Nothing;
+			}
+			else{
+				if(flag){
+					uart_state = Wait;
+				}
 			}
 
 			command_state = Begin;
 		}
 		else {
+
 			command_data[index_command++] = buffer[curr_index_buffer];
 		}
 		curr_index_buffer++;
@@ -41,13 +70,6 @@ void command_parser_fsm (){
 		break;
 	}
 }
-char strr[30];
-uint32_t ADC_value;
-enum Uart_state {
-		Init,
-		Wait
-};
-static enum Uart_state uart_state = Init;
 
 void uart_communiation_fsm () {
 	switch (uart_state){
@@ -64,16 +86,19 @@ void uart_communiation_fsm () {
 		}
 		break;
 	case Wait:
-		if(command_flag==1 && strcmp(command_data, "OK") == 0) {
-			uart_state = Init;
-			command_flag = 0;
-		}
-		else if (timer1_flag == 1){
+//		if(command_flag==1 && strcmp(command_data, "OK") == 0) {
+//			uart_state = Init;
+//			command_flag = 0;
+//		}
+		if (timer1_flag == 1){
 			//sprintf ( strr , "\r\n!ADC=%lu#\r\n", ADC_value );
 			HAL_UART_Transmit (&huart2 , (uint8_t *) strr, strlen( strr), 20);
 			set_Timer1(300);
 		}
 		break;
+	case Nothing:
+		break;
 	}
+
 
 }
